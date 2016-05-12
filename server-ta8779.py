4a2b115ea17f249185e21d5b2152763b75c1a62d
tarik90 <tarik90@users.noreply.github.com>
2016-05-12 11:14:59 -0500

    routing server to serve the update/serve the best route

--- server-ta8779.py
+++ server-ta8779.py
@@ -3,35 +3,18 @@
 import socket
 import sys
 import os
-import time
-import random
-import datetime
+import ipaddress
 
-
-#making a time.struct_time from	a given time string
-def formatTime(timeString):
-	date=None
-	try:
-		date=time.strptime(timeString,'%a, %d %b %Y %H:%M:%S %Z')
-	except ValueError:
-		try:
-			date=time.strptime(timeString)
-		except ValueError:
-			try:
-				date=time.strptime(timeString,'%A, %d-%b-%y %H:%M:%S %Z')
-			except ValueError:
-				try:
-					date=time.strptime(timeString,'%a %b %d %H:%M:%S %Y')
-				except ValueError:
-					return 1
-	return date
 
 
 host=''
 serverPort=int(sys.argv[1])
-serverName='NotSoAwesome 9.9'
 
 serverSocket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+routingTable=[]
+routingTable.append("A 0.0.0.0/0 100")
+
 
 try:
 	serverSocket.bind((host,serverPort))
@@ -40,128 +23,126 @@ except socket.error as e:
 print ('The server is runnning in port:' , serverPort)
 serverSocket.listen(1)
 
-timeNow= time.gmtime()
-timeNowStr= time.strftime('%a, %d %b %Y %H:%M:%S  %Z', timeNow)
-
-messageToAdd='Date: '+timeNowStr+'\r\n'
-messageToAdd+='Server: '+serverName+'\r\n'
-messageToAdd+='\r\n'
-
 
 #loop to accept the connection from any client and keep it open
 while 1:
 	connection, addr=serverSocket.accept()
-	print ('Connected to:'+addr[0]+': ' + str(addr[1]))
+
 	request = connection.recv(1024)
 	clientRequest=request.decode()
 	print (clientRequest)
+	response = clientRequest
+
+	#split the client request by \r\n to get individual part	
 	eachLines = clientRequest.split('\r\n')
 	lines=len(eachLines)
 
-	#check for 'If-Modified-Since' in the header and gets the line number it is in if exist
-	counter=0
-	lastModSince=None
-	for x in eachLines:
-		if 'If-Modified-Since' in x:
-			lastModSince=formatTime(eachLines[counter][19:])
-			break;
-		counter=counter+1
+	#check to see if first line is UPDATE
+	if (eachLines[0]=='UPDATE'):
+		# print ('ACK')
+		counter=1
+		while (counter < (lines-2)):
+			eachUpdate=eachLines[counter].split(" ")
+			router=eachUpdate[0] #A
+			eachIPmask=eachUpdate[1] #111.1.1.1/1
+			pathCost=eachUpdate[2] #100
+			routingTableLen=len(routingTable)
+			rCounter=0
+			
+			# loop to compare given CDIR with existing CIDR in the routing table
+			while(rCounter<routingTableLen):
+			    routes=routingTable[rCounter].split(" ")
+			    #routes[0] Routing table ---A
+			    #routes[1] Routing table ---111.11.11.1/1
+			    #routes[2] Routing table ---- Cost 100
 
-	#split the first line of header to sperate GET,filename and HTTP version if exist
-	eachPart=eachLines[0].split(' ')
-	parts=len(eachPart)
-	ifGET=eachLines[0][0:3]
+			    ip1=ipaddress.ip_network(routes[1])
+			    ip2=ipaddress.ip_network(eachIPmask)
 
-	#if correct request header 
-	if (parts ==3):
-		#check if header is GET
-		ifGET=eachLines[0][0:3]
-		if (ifGET=='GET'):
-			#check if HTTP is 1.1
-			if eachPart[parts-1]=='HTTP/1.1':
-				#check for the spported format of file
-					if any(s in eachPart[1] for s in ('.txt','.html','.htm','.jpeg','jpg')):
-						if ".txt" in eachPart[1]: 
-							fileType='text/plain'
-						elif ".html" in eachPart[1]: 
-							fileType='text/html'
-						elif ".htm" in eachPart[1]: 
-							fileType='text/html'
-						elif ".jpg" in eachPart[1]: 
-							fileType='image/jpeg'
-						elif ".jpeg" in eachPart[1]: 
-							fileType='image/jpeg'
+			    if(ip1.overlaps(ip2)):
+			    	ip1_prefix=(ip1.prefixlen)
+			    	ip2_prefix=(ip2.prefixlen)
 
-						#check existence of file
-						if os.path.isfile(eachPart[1][1:]):
-							dateOfModification=time.ctime(os.path.getmtime(eachPart[1][1:]))
-							formatDate=datetime.datetime.strptime(dateOfModification, '%a %b %d %H:%M:%S %Y').strftime('%a, %d %b %Y %H:%M:%S')
-							filestat = formatTime(time.ctime(os.path.getmtime(eachPart[1][1:])))
+			    #take action based on ip prefix & cost of the path
+			    	if(ip1_prefix>ip2_prefix):
+			    		if(int(routes[2])<=int(pathCost)):
+			    			routingTable.append(eachLines[counter])
+			    			routingTableLen=len(routingTable)
+			    		else:
+			    			routingTable[rCounter]=eachLines[counter]
+			    	elif(ip1_prefix<ip2_prefix):
 
-							#check for last-modified-since.if not exist lastModSince=None
-							if(lastModSince==None):
-								fileName=eachPart[1][1:]
-								fileSize=os.stat(fileName).st_size
-								response='\nHTTP/1.1 200 OK\r\n'
-								response+='Date: '+timeNowStr+'\r\n'
-								response+='Server: '+serverName+'\r\n'
-								response+='Last-Modified: '+formatDate+'  GMT\r\n'
-								response+='Content-Length: '+str(fileSize)+'\r\n'
-								response+='Content-Type: '+fileType+'\r\n'
-								response+='\r\n'
-								connection.sendall(response.encode())
-								openFile=open(fileName,'rb')
-								readFile=openFile.read(1024)
-								while (readFile):
-									connection.sendall(readFile)
-									readFile=openFile.read(1024)
-								openFile.close()
-							else:
-								if (filestat==lastModSince) or (filestat<lastModSince):
-									response='304 Not Modified'+'\r\n'
-									response+='Date: '+timeNowStr+'\r\n'
-									response+='Server: '+serverName+'\r\n'
-									connection.sendall(response.encode())
-								elif (filestat>lastModSince):
-									fileName=eachPart[1][1:]
-									fileSize=os.stat(fileName).st_size
-									response='\nHTTP/1.1 200 OK\r\n'
-									response+='Date: '+timeNowStr+'\r\n'
-									response+='Server: '+serverName+'\r\n'
-									response+='Last-Modified: '+formatDate+'  GMT\r\n'
-									response+='Content-Length: '+str(fileSize)+'\r\n'
-									response+='Content-Type: '+fileType+'\r\n'
-									response+='\r\n'
-									# response+='Line_based text data:'+fileType+'\r\n'
-									connection.sendall(response.encode())
-									# print(fileSize)
-									openFile=open(fileName,'rb')
-									readFile=openFile.read(1024)
-									while (readFile):
-										connection.sendall(readFile)
-										readFile=openFile.read(1024)
-									openFile.close()
+			    		if(int(routes[2])>=int(pathCost)):
+			    			routingTable.append(eachLines[counter])
+			    			routingTableLen=len(routingTable)
+			    		else:
+			    			#loop to remove all existing path that has higher cost and then break based on ip prefix
+			    			while True:
+			    				try:
+			    					routingTable.remove(eachLines[counter])
+			    				except ValueError:
+			    					break;
+			    			routingTableLen=len(routingTable)
+			    			break;
+			    	elif(ip1_prefix==ip2_prefix):
+			    		if(int(routes[2])>int(pathCost)):
+			    			routingTable[rCounter]=eachLines[counter]
+			    else:
+			    	routingTable.append(eachLines[counter])
+			    	routingTableLen=len(routingTable)
+			    
+			    rCounter+=1
 
-						else:
-							response='404 Not Found\r\n'
-							response+=messageToAdd
-							connection.sendall(response.encode())
-					else:
-						response='415 Unsupported Media Type\r\n'
-						response+=messageToAdd
-						connection.sendall(response.encode())
-			else:
-				response='505 HTTP Version Not Supported\r\n'
-				response+=messageToAdd
-				connection.sendall(response.encode())
-		else:
-			response="405 Method Not Allowed\r\n"
-			response+=messageToAdd
-			connection.sendall(response.encode())
-	else:
-		response='400 Bad Request\r\n'
-		response+=messageToAdd
-		connection.sendall(response.encode())
+			routingTable=list(set(routingTable))
+			counter+=1 
+
+		ack='ACK\r\n'
+		ack+='END\r\n'
+		
+		connection.sendall(ack.encode())
+			
+		#print the whole routing table
+		# print ("Routing table entries:")
+		# routingTableLen=len(routingTable)
+		# for i in range(0,routingTableLen):
+		# 	print (routingTable[i])
+
+	#check to see if first line is QUERY
+	if (eachLines[0]=='QUERY'):
+		counter=0
+		index=0
+		routerTableLength=len(routingTable)
+		minCost=9999999999999
+		longestPrefix=-1
+		closestRoutingIPs=[]
+
+		#loop to check the best path based on ip prefix and path cost
+		while(counter<routerTableLength):
+			eachPartOfRoute=routingTable[counter].split(" ")
+			ipAddress=eachLines[1]
+			IPmask=eachPartOfRoute[1]
+			cost=int(eachPartOfRoute[2])
+
+			ip=ipaddress.ip_network(IPmask)
+			ip_prefix=(ip.prefixlen)
+			
+			#check to see if requested query is in any of the routing table CIDRs
+			if ipaddress.ip_address(ipAddress) in ipaddress.ip_network(IPmask):
+				if (cost<=minCost):
+					if(ip_prefix>longestPrefix):
+						minCost=cost
+						longestPrefix=ip_prefix
+						# print("min:******")
+						# print(minCost)
+						index=counter
+			counter+=1
+
+		#best path
+		resultPath=routingTable[index].split(" ")
+		result="RESULT\r\n"
+		result+=eachLines[1]+" "+resultPath[0]+" "+resultPath[2]+'\r\n'
+		result+='END\r\n'
+
+		connection.sendall(result.encode())
 	
-	connection.close()
 	
